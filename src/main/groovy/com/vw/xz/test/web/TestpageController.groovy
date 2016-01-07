56a20d2b5537986d174dcae2be05f0d6df283281
Thomas Simmig <thomas.simmig@volkswagen.de>
2016-01-07 15:51:32 +0100

    Fixed deploylocal task

--- src/main/groovy/com/vw/xz/test/web/TestpageController.groovy
+++ src/main/groovy/com/vw/xz/test/web/TestpageController.groovy
@@ -1,14 +1,12 @@
 package com.vw.xz.test.web
 
 import org.springframework.stereotype.Controller
-import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 class TestpageController {
 
-    @RequestMapping(value="/", method = RequestMethod.GET)
+    @RequestMapping(value=[ '/', '/home' ])
     String showHomepage() {
         "index";
     }

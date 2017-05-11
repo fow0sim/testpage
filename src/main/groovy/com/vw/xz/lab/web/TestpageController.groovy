cef45b1cce754401da0635db9ef0a8798499e623
Thomas Simmig <thomas.simmig@volkswagen.de>
2017-05-11 09:37:42 +0200

    Added pages to display properties and ENVVARS

--- src/main/groovy/com/vw/xz/lab/web/TestpageController.groovy
+++ src/main/groovy/com/vw/xz/lab/web/TestpageController.groovy
@@ -1,14 +1,37 @@
 package com.vw.xz.lab.web
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class TestpageController {
 
     @RequestMapping(value=[ '/', '/home' ])
-    String showHomepage() {
+    String showHomepage(Model model) {
+        model.addAttribute('props', fetchProperties())
+        model.addAttribute('envars', fetchEnvars())
         "index"
+    }
+
+    @RequestMapping(value='/properties')
+    String showProperties(Model model) {
+        model.addAttribute('props', fetchProperties())
+        "properties"
+    }
+
+    @RequestMapping(value='/environment')
+    String showEnvironment(Model model) {
+        model.addAttribute('envars', fetchEnvars())
+        "environment"
+    }
+
+    static Properties fetchProperties() {
+        System.properties
+    }
+
+    static Map<String, String> fetchEnvars() {
+        System.getenv()
     }
 
 }
